d78475a70780004b149bfaad1f349c7e5c20e854
John van Hoof <john@vanhoof.family>
2020-04-04 16:37:14 +0100

    After Adding security scaffolding

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using leave_management.vanhoof.family.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(leave_management.vanhoof.family.Areas.Identity.IdentityHostingStartup))]
+namespace leave_management.vanhoof.family.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
